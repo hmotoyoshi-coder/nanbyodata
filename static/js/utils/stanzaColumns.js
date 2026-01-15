637069fe4b4ef7ea1f0a5113e5bd7ea32b3b173d
行事光 <h.gyoji.vrds@gmail.com>
2026-01-15 18:53:38 +0900

    fix column

--- static/js/utils/stanzaColumns.js
+++ static/js/utils/stanzaColumns.js
@@ -13,6 +13,7 @@ export const designatedIntractableDiseaseJaColumns = [
   { id: 'num_of_2021', label: '2021年', type: 'number' },
   { id: 'num_of_2022', label: '2022年', type: 'number' },
   { id: 'num_of_2023', label: '2023年', type: 'number' },
+  { id: 'num_of_2024', label: '2024年', type: 'number' },
 ];
 
 export const designatedIntractableDiseaseEnColumns = [
